be3b189b311b09069cd2afc2ec4bc9ffe89d8c3f
noixd <henrique.figueira.lopes@gmail.com>
2017-10-17 13:17:32 -0200

    Started migration from peewee to sqlalchemy

--- connectors/postgree.py
+++ connectors/postgree.py
@@ -1,10 +1,18 @@
-from playhouse import postgres_ext as pg_ext
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import expression
+from sqlalchemy.orm import sessionmaker
 
 
 class PostGreConnector:
 
     def __init__(self):
-        self.db = pg_ext.PostgresqlExtDatabase('henriquelopes.db')
+        engine = create_engine('postgresql://' + pg_user + ':' + pg_pswd + ' @' + pg_url + '/' + pg_db)
+        self.Base = declarative_base()
+        self.Base.metadata.bind = engine
+        DBsession = sessionmaker()
+        DBsession.bind = engine
+        self.session = DBsession()
 
     def CreateTable(self, Model):
         self.db.create_table(Model)
--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+pg_user = 'hlopes'
+pg_pswd = ''
+pg_db = 'cartoladb'
+pg_url = 'pg-cartola.c3pssepl8cax.us-east-2.rds.amazonaws.com:5432'
